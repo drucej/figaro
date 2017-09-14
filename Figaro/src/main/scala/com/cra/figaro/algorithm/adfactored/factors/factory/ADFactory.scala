2f8d3f17353b8b7049a580b32fdac7e29c204c4a
Dan Scofield <scofieldd@ainfosec.com>
2017-09-14 12:25:38 -0700

    Included debugging information when constraints are present

--- Figaro/src/main/scala/com/cra/figaro/algorithm/adfactored/factors/factory/ADFactory.scala
+++ Figaro/src/main/scala/com/cra/figaro/algorithm/adfactored/factors/factory/ADFactory.scala
@@ -153,9 +153,9 @@ object ADFactory {
       } else {
         comp.constraintFactors(Lower)
       }
-//      if (!constraint.isEmpty) {
-//        throw new IllegalStateException("Not sure what to do with Constraints here in ADFactory")
-//      }
+      if (!constraint.isEmpty) {
+        Console.err.println("Not sure what to do with Constraints here in ADFactory, solution might be wrong")
+      }
       //constraint ::: comp.nonConstraintFactors(parameterized)
       constraint.map({ f => f.mapTo[(Double, Double)]((d: Double) => (d, 0.0), SumProductDualSemiring())}) ::: makeFactors(comp.problem.collection, comp.element, parameterized).map(_.deDuplicate)
     }
