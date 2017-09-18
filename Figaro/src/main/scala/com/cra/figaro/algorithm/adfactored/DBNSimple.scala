746d6358df941da4d06dae023b39e88809943f06
Jeff Druce <jdruce@CRAs-MBP-2.cra.lan>
2017-09-18 09:38:48 -0400

    Updating DBNSimple

--- Figaro/src/main/scala/com/cra/figaro/algorithm/adfactored/DBNSimple.scala
+++ Figaro/src/main/scala/com/cra/figaro/algorithm/adfactored/DBNSimple.scala
@@ -36,6 +36,8 @@ class ModelV2(numTimesteps: Int) {
   val earthquakes: Array[Element[Boolean]] = Array.fill(numTimesteps)(earthquake)
   val johnCalls: Array[Element[Boolean]] = Array.fill(numTimesteps)(johnCall)
   val alarms: Array[Element[Boolean]] = Array.fill(numTimesteps)(alarm)
+//  val Queries: Array[Double] = Array.emptyDoubleArray(numTimesteps)
+
 
 
   //transition function
@@ -43,11 +45,11 @@ class ModelV2(numTimesteps: Int) {
 
     burglaries(i) = If(burglaries(i-1), Flip(0.05), Flip(0.01))
     earthquakes(i) = If(earthquakes(i-1), Flip(0.000001), Flip(0.0001))
-    alarms(i) = If(alarms(i-1), Flip(0.5), CPD(burglaries(i), earthquakes(i),
+    alarms(i) = CPD(burglaries(i), earthquakes(i),
       (false, false) -> Flip(0.001),
       (false, true) -> Flip(0.1),
       (true, false) -> Flip(0.9),
-      (true, true) -> Flip(0.99)))
+      (true, true) -> Flip(0.99))
     johnCalls(i) = CPD(alarms(i),
       false -> Flip(0.01),
       true -> Flip(0.7))
@@ -61,26 +63,30 @@ object DBNSimple {
   def main(args: Array[String]) {
 
     val numTimesteps = 2
-    val model = new Model(numTimesteps)
+    val model = new ModelV2(numTimesteps)
 
-//    val targets = model.alarms
-//    val alg = VariableElimination(targets: _*)
-//    alg.start
-//    alg.stop
+    //    val targets = model.alarms
+    //    val alg = VariableElimination(targets: _*)
+    //    alg.start
+    //    alg.stop
 
     val pJohnCallGivenAlarm = Constant(0.7)
-    val targets = model.Inds.last
-    val alg = ADVariableElimination.debugged(pJohnCallGivenAlarm, targets)
-    alg.start()
-    alg.kill()
+//    val targets = model.johnCalls.last
+
+    model.johnCalls(1).observe(true)
 
     for (i <- 0 until numTimesteps) {
-//      val targetProbability = alg.probability(model.alarms(i), true)
-      val (prob, deriv) = alg.getProbabilityAndDerivative(Alert, true)
-      println("probability: " + prob + "derivitive : " + deriv)
+      //      val targetProbability = alg.probability(model.alarms(i), true)
+      val alg = ADVariableElimination.debugged(model.pJohnCallGivenAlarm, model.johnCalls(i))
+      alg.start()
+      alg.stop()
+      val (prob, deriv) = alg.getProbabilityAndDerivative(model.johnCalls(i), true)
+      println("time step : " + i + " , probability: " + prob + " , derivitive : " + deriv)
+
+      alg.kill
+
     }
 
-    alg.kill
   }
 
 }
